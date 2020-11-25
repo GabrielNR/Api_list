f86001f458a66a386de80ed7d8c65ed5e96a25b0
Gabriel <gabrieldonascimento103@gmail.com>
2020-11-25 18:53:13 -0400

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
-    return response.json({ message: 'hello world' })
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
+});
+
+
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+
+app.put('/projects/:id', (request, response) => {
+        return response.json([
+            'Projeto 14',
+            'Projeto 2',
+            'Projeto 3',
+    ])
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+    'Projeto 3',
+])
 });
 
 app.listen(3333, () => {
