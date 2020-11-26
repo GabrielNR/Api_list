02643d53c60ba94f911d2ae1d595de7b9c5cf724
Gabriel <gabrieldonascimento103@gmail.com>
2020-11-25 20:09:38 -0400

    Aplicaçao funcional

--- src/index.js
+++ src/index.js
@@ -1,40 +1,74 @@
 const express = require('express');
+const { v4: uuid} = require('uuid')
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-    ]);
-});
+const projects = []
 
+app.get('/projects', (request, response) => {
+    const {title } = request.query;
+
+    const results = title 
+      ? projects.filter(project => project.title.includes(title)) 
+      : projects;
+  
+    return response.json(results)
+  });
 
 
 app.post('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    const { title, owner } = request.body
+
+    const project = { id: uuid(), title, owner };
+
+    projects.push(project);
+
+    return response.json(project)
+  
 });
 
 
 app.put('/projects/:id', (request, response) => {
-        return response.json([
-            'Projeto 14',
-            'Projeto 2',
-            'Projeto 3',
-    ])
-});
+    
+    const { id } = request.params
+    const { title, owner } = request.body
+
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+    return response.status(400).json( {error: "project not found"})
+}
+
+const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project)
+
+})
+
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-    'Projeto 3',
-])
-});
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0){
+    return response.status(400).json( {error: "project not found"})
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
+})
+
 
 app.listen(3333, () => {
     console.log('🚀️ Back-end started!')
